b40830b6d18024f48bfc5cbbcc200f5dfb66b039
Farah <faraho2013@gmail.com>
2024-12-14 19:59:24 -0600

    added actual and expected result assertions

--- features/steps/target_search_steps.py
+++ features/steps/target_search_steps.py
@@ -15,17 +15,18 @@ def click_on_cart_icon(context):
 
 @then('Verify message is shown')
 def verify_message_is_shown(context):
-    context.driver.find_element(By.XPATH, "//*[text()='Your cart is empty']")
-
+    expected_result = 'Your cart is empty'
+    actual_result = context.driver.find_element(By.XPATH, "//*[text()='Your cart is empty']").text
+    assert expected_result == actual_result, f' Expected text {expected_result} did not match {actual_result}'
 
 @when('Click on Sign in')
 def click_on_sign_in(context):
     context.driver.find_element(By.XPATH, "//*[text()='Sign in']").click()
     context.driver.find_element(By.XPATH, "//button[text()='Sign in']").click()
 
-
 @then('Verify Sign in form is opened')
 def verify_sign_in_form_is_opened(context):
-    context.driver.find_element(By.XPATH, "//*[@id='__next']/div/div/div/div[1]/div/h1/span")
-
+    expected_result = 'Sign into your Target account'
+    actual_result = context.driver.find_element(By.XPATH, "//*[@id='__next']/div/div/div/div[1]/div/h1/span").text
+    assert expected_result == actual_result, f'Expected text {expected_result} did not match {actual_result}'
 
